b9593e4dc8bd02bb0d9f8c714506f246ede9a4d4
Lahiru De Silva <lahirurashmitha00@gmail.com>
2024-02-23 18:41:15 +0530

    Set sessions

--- src/index.mjs
+++ src/index.mjs
@@ -1,12 +1,23 @@
 import express from "express";
 import router from "./routes/router.mjs";
 import cookieParser from "cookie-parser";
+import session from "express-session";
 
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser('secret'));
+app.use(session({ 
+    secret: 'xyz456',
+    // This will worck only user do enything 
+    // if this will true then session will set for all users for wisitin site
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: 60000 * 60
+    }
+ }));
 app.use(router);
 
 const PORT = process.env.PORT || 8000;
@@ -17,6 +28,9 @@ app.listen(PORT, () => {
 });
 
 app.get('/', (req, res) => {
+    req.session.visited = true;
+    console.log(req.session);
+    console.log(req.session.id); 
     // res.cookie("key", "value", { maxAge: 60000 * 60, signed: true });
     res.status(200).send("Hello express !");
 });
